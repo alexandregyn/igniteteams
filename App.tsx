6d3be0314f26d4d18740e1e29b794af742a91227
Alexandre Morais <alexandre_ferreira_gyn@hotmail.com>
2022-10-16 15:47:14 -0300

    Criado o component de header

--- App.tsx
+++ App.tsx
@@ -5,13 +5,19 @@ import theme from "@theme/index";
 
 import { Groups } from "@screens/groups";
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      { !fontsLoaded ? <Groups /> : <Loading /> }
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
