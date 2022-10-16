1947b1b8623c3f10f8f21fa28868afca305bfc0a
Alexandre Morais <alexandre_ferreira_gyn@hotmail.com>
2022-10-16 16:53:21 -0300

    Utilizado o flatList para listar os grupos

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+
 import { GruopCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+
 import { Container } from './styles';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState<Array<string>>(['Rocket']);
+
   return (
     <Container>
       <Header />
 
       <Highlight title='Turmas' subtitle='Jogue com a sua turma'/>
 
-      <GruopCard title='Galera do Ignite'/>
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <GruopCard title={item}/>
+        )}
+      />      
     </Container>
   );
 }
