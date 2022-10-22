86e61e112c6573511f1c6d5f89f9b2c729e2b2bc
Alexandre Morais <alexandre_ferreira_gyn@hotmail.com>
2022-10-22 11:18:56 -0300

    Tratativa de error

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -8,6 +8,8 @@ import { Input } from "@components/Input";
 
 import { Container, Content, Icon } from "./styles";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
+import { Alert } from "react-native";
 
 export function NewGroup() {
   const navigation = useNavigation();
@@ -15,10 +17,18 @@ export function NewGroup() {
 
   const handleNew = async () => {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Nova Turma', 'Informe o nome da turma.');;
+      }
       await groupCreate(group);
       navigation.navigate('players', { group });
     } catch (error) {
-      console.log(error);      
+      if (error instanceof AppError) {
+        Alert.alert('Nova Turma', error.message);
+      } else {
+        Alert.alert('Nova Turma', 'Não foi possível cadastra um nova Turma.');
+        console.error(error);
+      }
     }
   }
 
