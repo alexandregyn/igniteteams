3f256eda9385862bcc87b73bcbbd62195fa3dea6
Alexandre Morais <alexandre_ferreira_gyn@hotmail.com>
2022-10-22 11:54:21 -0300

    Criado método para adicionar pessoas no time

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
-import { FlatList } from "react-native";
+import { Alert, FlatList } from "react-native";
 import { useRoute } from "@react-navigation/native";
+
+import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
+import { AppError } from "@utils/AppError";
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
 
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
@@ -11,18 +15,44 @@ import { ListEmpty } from "@components/ListEmpty";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { PlayerCard } from "@components/PlayerCard";
 
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { playerGetByGroup } from "@storage/player/playerGetByGroup";
+
 type RouterParams = {
   group: string;
 }
 
-import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
-
 export function Players() {
+  const [newPlayerName, setNewPlayName] = useState('');
   const [team, setTeam] = useState('Time A');
   const [players, setPlayers] = useState([]);
 
   const router = useRoute();
   const { group } = router.params as RouterParams;
+
+  const handleAddPlayer = async () => {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.');
+    }
+
+    const newPlayer: PlayerStorageDTO = {
+      name: newPlayerName,
+      team
+    }
+
+    try {
+      await playerAddByGroup(newPlayer, group);
+      const players = await playerGetByGroup(group);
+      console.log(players);      
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Nova Pessoa', error.message);
+      } else {
+        Alert.alert('Nova Pessoa', 'Não foi possível incluir nova pessoa.');
+        console.error(error);
+      }
+    }
+  }
 
   return(
     <Container>
@@ -35,11 +65,12 @@ export function Players() {
 
       <Form>
         <Input 
+          onChangeText={setNewPlayName}
           placeholder="Nome da pessoa"
           autoCorrect={false}
         />
 
-        <ButtonIcon icon="add"/>
+        <ButtonIcon icon="add" onPress={handleAddPlayer}/>
       </Form>
 
       <HeaderList>
